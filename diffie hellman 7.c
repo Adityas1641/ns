8a1046544fd5ea386e001a0deb71c794a15c0eb5
Aditya Shinde <152697195+Adityas1641@users.noreply.github.com>
2024-05-07 01:15:29 +0530

    Add files via upload

---
+++ diffie hellman 7.c
@@ -0,0 +1,12 @@
+Aim :– In this experiment, we simulate certain "toy" versions of the popular 
+Diffie-Hellman secure key exchange protocol.
+
+Procedure: -
+
+STEP 1 : Firslty,choose a large prime number p and a generator g for that prime.
+STEP 2 : Secondly,both Alice and Bob generate their respective keys A and B. 
+And (ga,gb) for their keys respectively.
+STEP 3 : Both Alice and bob send exchange their ga,gb.
+STEP 4 : Finally,both calculate their public keys gab and gba repectively.
+STEP 5 : If both gab and gba are equal then Deffie-Hellman key exchange is 
+verified.
